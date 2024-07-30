c7d4d73a136d7f2020d6f58a91533102fc9d3ffb
unknown <inderece8031@gmail.com>
2024-07-30 19:07:33 +0530

    tried comparing hashed password and plaintextPassword

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
+// const bcrypt = require('bcrypt');
+// const { User } = require('./models/index');
 
 const apiRoutes = require('./routes/index');
 const bodyParser = require('body-parser');
@@ -9,9 +11,13 @@ const router = express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-router.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 
-const startServer = () => {
+const startServer = async () => {
+    // const hash = await User.findByPk(2);
+    // const response = bcrypt.compareSync("48579lkfjdbs4", hash.password);
+    // console.log(response);
+
     app.listen(PORT, () => {
         console.log(`Server started at port: ${PORT}`);
     });
