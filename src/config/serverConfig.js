f93c0921d773dbd1459f5f7cd30edc80d64c81fb
unknown <inderece8031@gmail.com>
2024-07-30 16:48:37 +0530

    Added password encryption using bcrypt

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,9 +1,12 @@
 const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const SALT = bcrypt.genSaltSync(10);
 
 module.exports = {
-    PORT
+    PORT,
+    SALT
 }
