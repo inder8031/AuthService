5775c87478c1579e52ed165fc42521a49a65e015
unknown <inderece8031@gmail.com>
2024-07-30 16:19:41 +0530

    setup bodyparser for reading request body and also setup router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
 
+const apiRoutes = require('./routes/index');
+const bodyParser = require('body-parser');
+
 const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+router.use('/api', apiRoutes);
 
 const startServer = () => {
     app.listen(PORT, () => {
