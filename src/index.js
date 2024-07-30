bd6583fb56ed52c7b39ac5517572dccc0523a6e1
unknown <inderece8031@gmail.com>
2024-07-30 16:05:25 +0530

    Revert "added routes for user"
    This reverts commit 40fa68c623ba7ba339c8d1470c54b24b42d8300f.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
 
-const apiRoutes = require('./routes/index');
-const bodyParser = require('body-parser');
-
 const app = express();
-const router = express.Router();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-router.use('/api', apiRoutes);
 
 const startServer = () => {
     app.listen(PORT, () => {
